4661f0b2faafadc8ca9d34c934a74e6bf5aed999
unknown <barishovaliana@gmail.com>
2022-01-02 22:05:45 +0100

    Add test examples

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -3,8 +3,26 @@ var sinon = require('sinon');
 var myObj = new MyClass();
 var chai = require('chai');
 var expect = chai.expect;
+const chaiaspromise = require("chai-as-promised");
+chai.use(chaiaspromise);
+const nock = require("nock");
 
-describe('Test suit', function () {    // test suit
+describe('Test suit', function () {
+    after(function () {
+        console.log("===================== After the test suit");
+    })
+    before(function () {
+        console.log("===================== Before the test suit");
+    });
+    afterEach(function () {
+        console.log("======= After each test case inside this test suit");
+    });
+    beforeEach(function () {
+        // Sinon wrappers must be restored before or after a test case.
+        // Hooks makes it easier implement
+        sinon.restore();
+        console.log("======= Before each test case inside this test suit");
+    });
     it('Test the add method', function () {
         expect(myObj.add(1, 2)).to.be.equal(3)
     });
@@ -18,12 +36,80 @@ describe('Test suit', function () {    // test suit
         expect(spy.calledWith(10, 20)).to.be.true;
     });
 
+    it('copy spy the add method', function () {
+        var spy = sinon.spy(myObj, "add");
+        var arg1 = 10, arg2 = 20;
+        myObj.callAnotherFn(arg1, arg2);
+        // sinon.assert.calledTwice(spy);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith(10, 20)).to.be.true;
+    });
+
     it('Spy the callback method', function () {
         var callback = sinon.spy();
         myObj.callTheCallback(callback);
         expect(callback.calledOnce).to.be.true;
+    });
+
+    it('Mock the sayHello method', function () {
+        var mock = sinon.mock(myObj);
+        var expectation = mock.expects('sayHello');
+        expectation.exactly(1);
+        expectation.withArgs('hello world');
+        myObj.callAnotherFn(10, 20);
+        mock.verify();
     })
-})
+});
+
+describe.skip("Test suit for stub", function () {
+    it("Stub the add method", function () {
+        var stub = sinon.stub(myObj, "add");
+        stub.withArgs(10, 20)
+            .onFirstCall()
+            .returns(100)
+            .onSecondCall()
+            .returns(200);
+        expect(myObj.callAnotherFn(10, 20)).to.be.equal(100);
+        expect(myObj.callAnotherFn(10, 20)).to.be.equal(200);
+    });
+});
+
+describe.skip("Test the promise", function () {
+    it("Promise test case", function (done) {
+        this.timeout(4000);
+        myObj.testPromise().then(function (result) {
+            expect(result).to.be.equal(6);
+            done();
+        });
+    });
+});
+
+describe.skip("Test the promise", function () {
+    it("Promise test case with chai-as-promised", function () {
+        this.timeout(0);
+        return expect(myObj.testPromise()).to.eventually.equal(6);
+    });
+});
+
+describe("XHR test suit", function () {
+    it("Mock and stub xhr call", function (done) {
+        var obj = { id: 123 };
+        const scope = nock("https://echo-service-new.herokuapp.com")
+            .post("/echo")
+            .reply(200, obj);
+        myObj
+            .xhrFn()
+            .then(function (result) {
+                // console.log(result);
+                expect(result).to.be.eql(obj);
+                // expect(result).to.be.eql({ id: 1234 });
+                done();
+            })
+            .catch(error => {
+                done(new Error("test case failed: " + error));
+            });
+    });
+});
 
 
 
--- src/myClass.js
+++ src/myClass.js
@@ -1,21 +1,65 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 class MyClass {
     constructor() {
         console.log('initiate');
     }
 
-    add(arg1, arg2) {
-        var result;
-        result = arg1 + arg2;
-        return result;
+    sayHello(str) {
+        console.log(str);
     }
 
-    callAnotherFn(arg1, arg2) {
+    add(arg1, arg2) {
+        if (1) {
+            var result;
+            result = arg1 + arg2;
+            return result;
+        } else {
+            return 2;
+        }
+
+    }
+
+    callAnotherFn(arg1, arg2) {  // Для того чтобы протестировать ф-ю callAnotherFn независимо на внутр.вызов ф-ии sayHello() исп-ся mock
+        this.sayHello('hello world');
         var result = this.add(arg1, arg2);
         return result;
     }
 
     callTheCallback(callback) {
         callback();
+    }
+
+    testPromise() {
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(3), 6000);
+        }).then(function (result) {
+            return result * 2;
+        })
+    }
+
+    xhrFn() {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open("post", "https://echo-service-new.herokuapp.com/echo", true);
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.status);
+                    }
+                }
+            };
+            xhr.send();
+        })
+            .then(function (result) {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
     }
 }
 
